474ba6252c8c55ba7251a2bdb428e057b8a4f1b2
CREDO23 <thierrybakera12@gmail.com>
2023-02-16 09:28:52 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+int intype;
+char chartype;
+long longtype;
+long long int longinttype;
+float floattype;
+
+printf("Size of a char: %i byte(s) \n", sizeof(chartype));
+printf("Size of a int: %i byte(s)\n", sizeof(intype));
+printf("Size of a long: %i byte(s)\n", sizeof(longtype));
+printf("Size of a long long int: %i byte(s)\n", sizeof(longinttype));
+printf("Size of a float: %i byte(s)\n", sizeof(floattype));
+
+}
