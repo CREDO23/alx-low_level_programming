e765a2dca92ef5785750c2a0192cf443ecd68e52
Thierry CH <thierrybakera12@gmail.com>
2023-02-16 19:32:05 +0200

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,9 +13,11 @@ long long int longinttype;
 float floattype;
 
 printf("Size of a char: %i byte(s) \n", sizeof(chartype));
-printf("Size of a int: %i byte(s)\n", sizeof(intype));
-printf("Size of a long: %i byte(s)\n", sizeof(longtype));
+printf("Size of an int: %i byte(s)\n", sizeof(intype));
+printf("Size of a long int: %i byte(s)\n", sizeof(longtype));
 printf("Size of a long long int: %i byte(s)\n", sizeof(longinttype));
 printf("Size of a float: %i byte(s)\n", sizeof(floattype));
+  
+return (0)
 
 }
