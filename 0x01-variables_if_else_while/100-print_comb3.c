3ab943951eb36ee71e6c593a23f49056329697fa
CREDO23 <thierrybakera12@gmail.com>
2023-02-17 05:11:06 -0800

    combinason

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ * Return Always 0
+ */
+int main(void)
+{
+
+int i,j;
+
+for ( i = 0; i < 10; i++ )
+{
+
+for ( j = i + 1; j < 10; j++ )
+{
+
+if( !( i == 0 && j == 1 ) )
+{
+	putchar(' ');
+}
+
+putchar('0' + i);
+
+putchar('0' + j);
+
+if ( !( i == 8 && j == 9 ) )
+{
+	putchar(',');
+}
+
+}
+
+}
+
+putchar('\n');
+
+return (0);
+}
