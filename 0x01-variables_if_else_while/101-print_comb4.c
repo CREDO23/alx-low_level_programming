27b287ce5ce3537fa8055978e9a9bf1e11ea9522
CREDO23 <thierrybakera12@gmail.com>
2023-02-17 05:27:07 -0800

    fix betty style

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,18 +7,18 @@
 int main(void)
 {
 
-int i,j,k;
+int i, j, k;
 
-for ( i = 0; i < 8; i++ )
+for (i = 0; i < 8; i++)
 {
 
-for ( j = i + 1; j < 9; j++ )
+for (j = i + 1; j < 9; j++)
 {
 
-for ( k = j + 1; k < 10; k++ )
+for (k = j + 1; k < 10; k++)
 {
 
-if( !( i == 0 && j == 1 && k ==2 ) )
+if(!(i == 0 && j == 1 && k ==2))
 {
 	putchar(' ');
 }
@@ -27,7 +27,7 @@ putchar('0' + i);
 putchar('0' + j);
 putchar('0' + k);
 
-if ( !( i == 7 && j == 8 && k == 9 ) )
+if (!(i == 7 && j == 8 && k == 9))
 {
 	putchar(',');
 }
