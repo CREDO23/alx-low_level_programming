2dfbd9737283a7eb0068370bb961676d8a2b3219
CREDO23 <thierrybakera12@gmail.com>
2023-02-21 04:03:53 -0800

    fix

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,24 +2,24 @@
 
 /**
  * print_sign - Entry point
- * @n: The number to be checked
+ * @c: The number to be checked
  * Return: 1 if n is greater than 0, -1 if n is less than 0 otherwise 0
  */
 int print_sign(int c)
 {
 	if (c > 0)
 	{
-		printf("%d\n", '+');
+		printf("%d\n", +);
 		return (1);
 	}
 	else if (c < 0)
 	{
-		printf("%d\n", '-');
+		printf("%d\n", -);
 		return (-1);
 	}
 	else
 	{
-		printf("%d\n", '0');
+		printf("%d\n", 0);
 		return (0);
 	}
 }
