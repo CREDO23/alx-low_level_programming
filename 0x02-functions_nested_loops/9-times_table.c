5865ea4b53e48f35257bf2517f430da6fc663908
CREDO23 <thierrybakera12@gmail.com>
2023-02-21 05:22:49 -0800

    fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,16 +12,16 @@ void times_table(void)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			if (i != 9 || j != 9)
+			if (j != 9)
 				if (i * j < 10)
-					printf("%d,  ", i * j);
+					printf("  %d,", i * j);
 				else	
-					printf("%d, ", i * j);
+					printf(" %d,", i * j);
 			else	
 				if (i * j < 10)
-					printf("%d,  ", i * j);
+					printf("  %d,", i * j);
 				else		
-					printf("%d, ", i * j);
+					printf(" %d,", i * j);
 		}
 		printf("\n");
 	}
