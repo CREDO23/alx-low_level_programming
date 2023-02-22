828a23dd3889a15d3cc837152b5ce517ca9d786b
CREDO23 <thierrybakera12@gmail.com>
2023-02-21 22:58:27 -0800

    debugging

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -20,9 +20,13 @@ int largest_number(int a, int b, int c)
     {
         largest = b;
     }
-    else
+    else if (c > a && c > b)
     {
         largest = c;
+    }
+    else
+    {
+	    return (0);
     }
 
     return (largest);
