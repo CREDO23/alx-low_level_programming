bdfcce0e55c44e5ee6de65c3262f550984527e76
CREDO23 <thierrybakera12@gmail.com>
2023-02-23 02:16:29 -0800

    print trialgle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * print_triangle - The funtion to print the trinanle
+ * @size: The size of the triangle
+ * Return: Nothing
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+
+			for (j = 0; j <= size; j++)
+			{
+				if (j >= (size - i))
+					_putchar('#');
+				else
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+	else
+		_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3)
+			printf("%s ", 'Fizz');
+		else if (i % 5)
+			printf("%s ", 'Buzz');
+		else if (i % 3 && i % 5)
+			printf("%s ", 'FizzBuzz');
+		else
+			printf("%d", i);
+	}
+	printf("\n");
+}
