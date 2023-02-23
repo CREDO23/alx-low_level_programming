68192fdbe619bac477e6a6dd292c8c4860d7a4bf
CREDO23 <thierrybakera12@gmail.com>
2023-02-23 04:58:21 -0800

    fix

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <ctype.h>
 #include "main.h"
 
 /**
@@ -9,7 +8,7 @@
  */
 int _isdigit(int c)
 {
-	if (isdigit(i))
+	if (i > '0' && i < '9')
 		return (1);
 	else
 		return (0);
