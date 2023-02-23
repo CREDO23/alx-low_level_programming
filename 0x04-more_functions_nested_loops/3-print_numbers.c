d20036028b2190c40646cd3f748c9785bc03fd13
CREDO23 <thierrybakera12@gmail.com>
2023-02-23 01:12:53 -0800

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * print_numbers - The function to prints numbers
+ * Retrun always 0
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * print_most_numbers - The function to print most number
+ * Return: Always 0
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 || i != 4)
+			_putchar(i);
+	}
+	_putchar('\n');
+}
