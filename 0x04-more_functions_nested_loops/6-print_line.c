e44998d5ff344eeb939eef17ab674581c5fde0e6
CREDO23 <thierrybakera12@gmail.com>
2023-02-23 01:33:59 -0800

    print nuline

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * print_line - The funtion to print the line
+ * @n: The length of the line
+ * Return: Nothing
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar(_);
+		}
+		_putchar('\n');
+	}
+	else
+		_putchar('\n');
+}
