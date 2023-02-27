8c8338371d2a327e6c30fe6d0009204c3f825fae
CREDO23 <thierrybakera12@gmail.com>
2023-02-27 12:57:41 -0800

    atoi

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ *  _atoi - The atoi funtion
+ *  @s: The string to be formated
+ *  Return: Numberrrrrrrr
+ */
+int _atoi(char *s)
+{
+	int prev = 0;
+	int next = 0;
+	int i;
+	int signindex;
+
+	for (i = 0; i != '\0'; i++)
+	{
+		if (s[i] > '9' || s[i] < '0')
+		{
+			if (res != 0)
+				prev++;
+			continue;
+		}
+		else
+		{
+			if (next - prev == 1)
+			{
+				if (res == 0)
+					signindex = i - 1;
+				res = res * 10 + s[i] - '0';
+				prev = i;
+				next = i + 1;
+			}
+			else
+				return (res);
+		}
+	}
+
+	while (s[signindex] == 45 || s[signindex] == 43 || s[signindex] == 11)
+	{
+		if (s[signindex] == 45)
+			res = -res;
+		signindex++;
+	}
+
+	return (res);
+}
