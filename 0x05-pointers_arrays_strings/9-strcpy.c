2321a4184623f03dea04f086aa527fc3502c24e9
CREDO23 <thierrybakera12@gmail.com>
2023-02-27 07:18:37 -0800

    src dest

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * *_strcpy - The funtion
+ * @dest: The destination
+ * @src: The source
+ * Return: THe dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	*dest = *src;
+	return (*dest);
+}
