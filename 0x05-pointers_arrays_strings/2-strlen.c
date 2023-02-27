bf25e4e64688b971d95d9410a9acc3e2c5a79d2d
CREDO23 <thierrybakera12@gmail.com>
2023-02-27 04:12:40 -0800

    lenth string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - The funtion to display the length
+ * @s: The pointer to the string
+ * Return: The length of s
+ */
+int _strlen(char *s)
+{
+	char str = *s;
+	int i;
+
+	for (i = 0; str[i] != '\0'; ++i);
+	return (i)
+}
