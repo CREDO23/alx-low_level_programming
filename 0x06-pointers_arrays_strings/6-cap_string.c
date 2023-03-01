4c6ed1bb960c7bbc02878637c541660fd398c56b
CREDO23 <thierrybakera12@gmail.com>
2023-03-01 08:38:00 -0800

    fix

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * *cap_string- The funtion
+ * @s: The string
+ * Return: NTHNG
+ */
+char *cap_string(char *)
+{
+	int i,j = 0;
+
+	int *sep = ".!?,\n\t;\"(){}"
+
+	for (
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
@@ -10,5 +10,8 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+char *rot13(char *);
 
 #endif /* MAIN_H */
