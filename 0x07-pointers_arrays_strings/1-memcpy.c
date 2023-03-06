2a0fc1ffbdd6f74acb37cb2e24386d5f4369c60c
CREDO23 <thierrybakera12@gmail.com>
2023-03-06 11:25:58 -0800

    copymem

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,11 +11,10 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
 
-	while (i < n && *(dest + i))
+	for (i = 0; i < n; i++)
 	{
 		*(dest + i) = *(src + i)
 
-		i++;
 	}
 
 	return (dest);
