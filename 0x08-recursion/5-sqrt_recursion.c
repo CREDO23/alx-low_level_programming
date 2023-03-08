56e65d8cec1f529f5423a4d130812c5d0e31522e
CREDO23 <thierrybakera12@gmail.com>
2023-03-08 03:22:40 -0800

    the power

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _sqrt - The funtion that helps us to find the square of a number
+ * @sqr: THe square
+ * @n: THe number
+ */
+unsigned int _sqrt(int sqr, int n)
+{
+	if (sqr * sqr == n)
+		return (sqr);
+	if (sqr >= n)
+		return (-1);
+
+	return (_sqrt(sqr + 1), n);
+}
+
+/**
+ * _sqrt_recursion- The square funtion
+ * @n: The number
+ * Return: The square of n
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	return (_sqrt(0, n));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,6 +7,7 @@ int _putchar(char c);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
 int factorial(int n);
 void _puts_recursion(char *s);
 
