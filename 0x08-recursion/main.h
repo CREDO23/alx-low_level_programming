2ca0efe91c6261a4d8401994de389c6024f3a70f
CREDO23 <thierrybakera12@gmail.com>
2023-03-08 02:08:38 -0800

    power

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,7 +6,7 @@
 int _putchar(char c);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
-int main(void)
+int _pow_recursion(int x, int y);
 int factorial(int n);
 void _puts_recursion(char *s);
 
