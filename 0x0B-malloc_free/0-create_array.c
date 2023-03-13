17db3acb608e0b71081ba4f10f877cfb1f8113c4
CREDO23 <thierrybakera12@gmail.com>
2023-03-13 11:27:31 -0700

    create array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+#include "main"
+
+/**
+ * *create_array - Create an array
+ * @size: THe size of the table
+ * @c: THe character
+ * Return: A poiter to array or NULL
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr = malloc(size);
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+
+	return (arr);
+}
