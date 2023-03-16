2554aca93d61fb166f0a12e9f1a2b651502ccdee
CREDO23 <thierrybakera12@gmail.com>
2023-03-16 20:16:09 +0200

    fix

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,8 +3,9 @@
 /**
  * *malloc_checked - The funtion
  * @b: The size of memory to allocate
+ * Return: A pointer
  */
 void *malloc_checked(unsigned int b)
 {
-	return (malloc(b));
+	return ((void *)malloc(b));
 }
