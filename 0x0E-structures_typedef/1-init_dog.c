fd334df9ce6ee3ee71ebe8322e5a9297a34a0bcf
CREDO23 <thierrybakera12@gmail.com>
2023-03-20 15:16:08 +0200

    add init dog funtion

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - The init dog funtion
+ * @d: The dog arg
+ * @name: The name
+ * @age: The age
+ * @owner: The owner
+ * Return: Nothing
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
