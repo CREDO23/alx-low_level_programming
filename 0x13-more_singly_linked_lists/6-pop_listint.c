06a9cf64aed9c733eea27c61d1750da7f9903f01
CREDO23 <thierrybakera12@gmail.com>
2023-04-03 21:24:51 +0200

    fix

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -8,11 +8,14 @@
  */
 int pop_listint(listint_t **head)
 {
-	listint_t *tmp = (*head)->next;
-	int data = (*head)->n;
+	listint_t *tmp;
+	int data;
 
 	if (head == NULL || *head == NULL)
 		return (0);
+
+	tmp = (*head)->next;
+	data = (*head)->n;
 
 	free(*head);
 
