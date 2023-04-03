98112b3cb031a0ced0523a44decb7a42aa38c529
CREDO23 <thierrybakera12@gmail.com>
2023-04-03 21:12:31 +0200

    add pop_listint function

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * pop_listint - The function to pop a ll
+ * @head: THe address of the head of the ll
+ *
+ * Return: The head's data
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp = (*head)->next;
+	int data = (*head)->n;
+
+	free(*head);
+
+	*head = tmp;
+
+	return (data);
+
+
+}
