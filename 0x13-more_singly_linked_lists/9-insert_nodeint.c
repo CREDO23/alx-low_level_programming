b64f143777f3e35bb90031bc95ee8ac92230476f
CREDO23 <thierrybakera12@gmail.com>
2023-04-03 23:28:56 +0200

    fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -37,7 +37,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		tmpl = tmpl->next;
 	}
 
-	if (index < idx)
+	if (index < idx - 1)
 		return (NULL);
 
 	return (new);
