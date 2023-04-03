b2b8fd24fe2e0c4f49997e144ebd2de80b4d417a
CREDO23 <thierrybakera12@gmail.com>
2023-04-03 22:43:22 +0200

    add the add_at_index function

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - The function
+ * @head: THe head
+ * @idx: The index of the list where the node should be added
+ * @n: THe data
+ *
+ * Return: The new node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int index = 1;
+	listint_t *tmpl = *head;
+	listint_t *tmpr;
+
+	listint_t *new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	while (tmpl)
+	{
+		if (index == idx)
+		{
+			tmpr = tmpl->next;
+			tmpl->next = new;
+			new->next = tmpr;
+		}
+
+		index++;
+		tmpl = tmpl->next;
+	}
+
+	if (index < idx)
+		return (NULL);
+
+	return (new);
+}
