5626068b517fd95149e85d7f5a6a17a3eade608c
CREDO23 <thierrybakera12@gmail.com>
2023-04-03 20:53:10 +0200

    add free_listint2 function

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - THe function to free a ll
+ * @head: The address of the head of the ll
+ *
+ * Return: Nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *nxt;
+
+	while (*head)
+	{
+		nxt = (*head)->next;
+		free(*head);
+
+		*head = nxt;
+	}
+
+	*head = NULL;
+}
