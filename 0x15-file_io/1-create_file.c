94d5e3f4efb1a33ba90d9b8643995b19032d59e2
CREDO23 <thierrybakera12@gmail.com>
2023-04-10 09:50:49 +0200

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,16 +12,19 @@ int create_file(const char *filename, char *text_content)
 {
 	int len, fdo, wbytes, i = 0;
 
-	if (filename == NULL || text_content == NULL)
+	if (filename == NULL)
 		return (-1);
 
-	while (text_content[i++])
+	if (text_content != NULL)
+	{
+		while (text_content[i++])
 		len++;
+	}
 
 	fdo = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wbytes = write(fdo, text_content, len);
 
-	if (fdo < 0 || wbytes < 0)
+	if (fdo < -1 || wbytes == -1)
 		return (-1);
 
 	close(fdo);
