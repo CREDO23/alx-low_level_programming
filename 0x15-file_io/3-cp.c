ebea9b33f0fb1f1775e2e947f73a1a5adc67c906
CREDO23 <thierrybakera12@gmail.com>
2023-04-10 15:50:52 +0200

    fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -51,7 +51,7 @@ char *create_buff(char *filename)
  */
 int main(int argc, char **argv)
 {
-	int fdo1, fdo2, rbytes1;
+	int fdo1, fdo2, rbytes, wbytes;
 	char *buffer;
 
 	if (argc < 3)
@@ -61,25 +61,32 @@ int main(int argc, char **argv)
 	}
 
 	buffer = create_buff(argv[1]);
-
 	fdo1 = open(argv[1], O_RDONLY);
-	if (!fdo1 || fdo1 == -1)
-	{
-		dprintf(STDERR_FILENO, "%s %s\n", "Error: Can't read from file", argv[1]);
-		exit(98);
-	}
-
-	rbytes1 = read(fdo1, buffer, 1024);
-
+	rbytes = read(fdo1, buffer, 1024);
 	fdo2 = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
-	if (!fdo2 || fdo2 == -1)
-	{
-	dprintf(STDERR_FILENO, "%s %s\n", "Error: Can't write to NAME_OF_THE_FILE"
-			, argv[2]);
-	exit(99);
-	}
 
-	write(fdo2, buffer, rbytes1);
+	do {
+		if (fdo1 == -1 || rbytes == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
+
+		wbytes = write(fdo2, buffer, rbytes);
+		if (fdo2 == -1 || wbytes == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+
+		rbytes = read(fdo1, buffer, 1024);
+		fdo2 = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (rbytes > 0);
 
 	close_file(fdo1);
 	close_file(fdo2);
