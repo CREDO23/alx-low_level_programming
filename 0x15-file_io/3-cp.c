558f3c943ad44c1c6f1ad5ac0a7d248cef497e48
CREDO23 <thierrybakera12@gmail.com>
2023-04-10 16:31:35 +0200

    fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,16 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-char *create_buffer(char *file);
+char *create_buff(char *filename);
 void close_file(int fd);
 
 /**
  * create_buffer - Allocates 1024 bytes for a buffer.
- * @file: The name of the file buffer is storing chars for.
+ * @file: The name of the file buffer is sfdo2ring chars for.
  *
- * Return: A pointer to the newly-allocated buffer.
+ * Return: A pointer fdo2 the newly-allocated buffer.
  */
-char *create_buffer(char *file)
+char *create_buff(char *filename)
 {
 	char *buffer;
 
@@ -20,7 +20,7 @@ char *create_buffer(char *file)
 	if (buffer == NULL)
 	{
 		dprintf(STDERR_FILENO,
-			"Error: Can't write to %s\n", file);
+			"Error: Can't write fdo2 %s\n", filename);
 		exit(99);
 	}
 
@@ -28,8 +28,8 @@ char *create_buffer(char *file)
 }
 
 /**
- * close_file - Closes file descriptors.
- * @fd: The file descriptor to be closed.
+ * close_file - Closes file descripfdo2rs.
+ * @fd: The file descripfdo2r fdo2 be closed.
  */
 void close_file(int fd)
 {
@@ -45,59 +45,54 @@ void close_file(int fd)
 }
 
 /**
- * main - Copies the contents of a file to another file.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
+ * main - Copies the contents of a file fdo2 another file.
+ * @argc: The number of arguments supplied fdo2 the program.
+ * @argv: An array of pointers fdo2 the arguments.
  *
- * Return: 0 on success.
- *
- * Description: If the argument count is incorrect - exit code 97.
- *              If file_from does not exist or cannot be read - exit code 98.
- *              If file_to cannot be created or written to - exit code 99.
- *              If file_to or file_from cannot be closed - exit code 100.
+ * Return: 0 on success
  */
 int main(int argc, char *argv[])
 {
-	int from, to, r, w;
+	int fdo1, fdo2, rbytes, wbytes;
 	char *buffer;
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_fdo1 file_fdo2\n");
 		exit(97);
 	}
 
-	buffer = create_buffer(argv[2]);
-	from = open(argv[1], O_RDONLY);
-	r = read(from, buffer, 1024);
-	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	buffer = create_buff(argv[2]);
+	fdo1 = open(argv[1], O_RDONLY);
+	rbytes = read(fdo1, buffer, 1024);
+	fdo2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
-		if (from == -1 || r == -1)
+		if (fdo1 == -1 || rbytes == -1)
 		{
 			dprintf(STDERR_FILENO,
-				"Error: Can't read from file %s\n", argv[1]);
+				"Error: Can't read fdo1 file %s\n", argv[1]);
 			free(buffer);
 			exit(98);
 		}
 
-		w = write(to, buffer, r);
-		if (to == -1 || w == -1)
+		wbytes = write(fdo2, buffer, rbytes);
+		if (fdo2 == -1 || wbytes == -1)
 		{
 			dprintf(STDERR_FILENO,
-				"Error: Can't write to %s\n", argv[2]);
+				"Error: Can't write fdo2 %s\n", argv[2]);
 			free(buffer);
 			exit(99);
 		}
 
-		r = read(from, buffer, 1024);
-		to = open(argv[2], O_WRONLY | O_APPEND);
+		rbytes = read(fdo1, buffer, 1024);
+		fdo2 = open(argv[2], O_WRONLY | O_APPEND);
 
-	} while (r > 0);
+	} while (rbytes > 0);
 
 	free(buffer);
-	close_file(from);
-	close_file(to);
+	close_file(fdo1);
+	close_file(fdo2);
 
 	return (0);
 }
