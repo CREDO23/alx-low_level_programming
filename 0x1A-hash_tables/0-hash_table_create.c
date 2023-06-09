e6e13bf76aa72d67a3df34ccc34b8be553799fcf
CREDO23 <thierrybakera12@gmail.com>
2023-06-09 11:20:43 +0200

    add the create hash function

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_t *hash_table_create - Create a hash table
+ * @size: The size of the hash table
+ *
+ * Return: The newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+	unsigned long int i;
+
+	table = malloc(sizeof(hash_table_t));
+
+	if (table == NULL)
+		return (NULL);
+
+	table->size = size;
+
+	table->array = (hash_node_t **)calloc(size, sizeof(hash_node_t));
+
+	if (table->array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		table->array[i] = NULL;
+	}
+
+	return (table);
+}
