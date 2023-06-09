e9bcf45b35bdb2af85f9c1495aa166ebb9c8dee7
CREDO23 <thierrybakera12@gmail.com>
2023-06-09 22:13:37 +0200

    fix the set method for the hash tab;e

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,37 +8,43 @@
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *node;
-	unsigned int index;
-	hash_node_t *head;
+	hash_node_t *new;
+	char *value_copy;
+	unsigned long int index, i;
 
-	node = malloc(sizeof(hash_node_t));
-	if (node == NULL)
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
-	node->key = (char *)key;
-	node->value = (char *)value;
-	node->next = NULL;
+	value_copy = strdup(value);
+	if (value_copy == NULL)
+		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
-
-	if (index)
+	for (i = index; ht->array[i]; i++)
 	{
-		if (ht->array[index] == NULL)
+		if (strcmp(ht->array[i]->key, key) == 0)
 		{
-			ht->array[index] = node;
-		}
-		else
-		{
-			head = ht->array[index];
-
-			while (head->next != NULL)
-			{
-				head = head->next;
-			}
-
-			head->next = node;
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
 		}
 	}
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		free(value_copy);
+		return (0);
+	}
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = value_copy;
+	new->next = ht->array[index];
+	ht->array[index] = new;
+
 	return (1);
 }
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 /**
  * struct hash_node_s - Node of a hash table
