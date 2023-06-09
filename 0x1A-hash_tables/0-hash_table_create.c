bafd066327bb32fe4db994160c8f2416cf33583d
CREDO23 <thierrybakera12@gmail.com>
2023-06-09 11:28:14 +0200

    fix create hash table function

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,7 +18,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	table->size = size;
 
-	table->array = (hash_node_t **)calloc(size, sizeof(hash_node_t *));
+	table->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
 
 	if (table->array == NULL)
 		return (NULL);
