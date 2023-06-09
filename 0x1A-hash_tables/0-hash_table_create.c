4d798ae817e3b637a7619b6c005e80f194fcf763
CREDO23 <thierrybakera12@gmail.com>
2023-06-09 11:38:52 +0200

    add the djb2 function use as hash function to generate key

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,34 +1,32 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_create - A function that creates a hash table
- * @size: The size of the array
+ * hash_table_create - Create a hash table
+ * @size: The size of the hash table
  *
- * Return: If an error occurs - NULL
- *         Otherwise - a pointer to the new hash table
+ * Return: The newly created hash table
  */
-
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table_created;
+	hash_table_t *table;
 	unsigned long int i;
 
-	/* allocate space for the hash table */
-	hash_table_created = malloc(sizeof(hash_table_t));
+	table = malloc(sizeof(hash_table_t));
 
-	if (hash_table_created == NULL)
+	if (table == NULL)
 		return (NULL);
 
-	hash_table_created->size = size;
-	hash_table_created->array = malloc(sizeof(hash_node_t *) * size);
+	table->size = size;
 
-	if (hash_table_created->array == NULL)
+	table->array = (hash_node_t **)malloc(sizeof(hash_node_t *) * size);
+
+	if (table->array == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
 	{
-		hash_table_created->array[i] = NULL;
+		table->array[i] = NULL;
 	}
 
-	return (hash_table_created);
+	return (table);
 }
