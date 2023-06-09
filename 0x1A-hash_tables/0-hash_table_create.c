cbfe9af4c19147bebd9ab9f4d09976254a8996c4
CREDO23 <thierrybakera12@gmail.com>
2023-06-09 11:31:10 +0200

    fix create hash table function

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,7 +1,7 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_t *hash_table_create - Create a hash table
+ * hash_table_create - Create a hash table
  * @size: The size of the hash table
  *
  * Return: The newly created hash table
